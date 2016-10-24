1ba09adc91ab9672d6d8c4410d72531897b1a711
Ru Tan <jutan2012@gmail.com>
2016-10-24 14:39:30 +0200

    initila commit

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 (function(){
-$("button").click(function(){
-	var go = $.ajax(){
-		url:'http://192.168.1.21:3000/places',
-		method:'GET,'
-	}
+	console.log("hello world");
+	$("#start").click(function(){
+		console.log("lala");
+		var url ="http://192.168.1.21:3000/places" ; 
+		$.ajax(url).done(function(){
+			$.get(url,function(response){
+				console.log(response.places);
+				for (var i = 0; i < response.places.length; i++) {
+					response.places[i]
+					$("#list").append("<li>"+response.places[i]+"</li>")
+				}
+			});
 
-});
-});
+
+		});
+		$.ajax(url).fail(function(){
+			alert("fail");
+
+		});
+		$.ajax(url).always(function(){
+			alert("always");
+
+		});
+
+
+
+
+	});
+})();
